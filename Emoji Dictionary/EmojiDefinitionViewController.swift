9d75172c18504d8047055df762ed4871067abd6f
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-21 22:14:37 -0800

    add definitions to emojis

--- Emoji Dictionary/EmojiDefinitionViewController.swift
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -12,11 +12,42 @@ class EmojiDefinitionViewController: UIViewController {
     
     @IBOutlet weak var emojiLabel: UILabel!
     var emoji = ""
-
+    @IBOutlet weak var emojiDefinitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         emojiLabel.text = emoji
+        
+        if emoji == "😀" {
+            emojiDefinitionLabel.text = "OG Smiley"
+        } else if emoji == "💩" {
+            emojiDefinitionLabel.text = "Smiling Poop"
+        } else if emoji == "🤢" {
+            emojiDefinitionLabel.text = "Ready-to-Vomit Smiley"
+        } else if emoji == "😎" {
+            emojiDefinitionLabel.text = "Coolest Smiley"
+        } else if emoji == "😆" {
+            emojiDefinitionLabel.text = "The Laughing Smiley"
+        } else if emoji == "👻" {
+            emojiDefinitionLabel.text = "Ghost"
+        } else if emoji == "🍺" {
+            emojiDefinitionLabel.text = "Beer"
+        } else if emoji == "🥃" {
+            emojiDefinitionLabel.text = "Whiskey Neat"
+        } else if emoji == "🍻" {
+            emojiDefinitionLabel.text = "Cheers Beers"
+        } else if emoji == "🍜" {
+            emojiDefinitionLabel.text = "A Bowl of Noodle Soup"
+        } else if emoji == "🏎" {
+            emojiDefinitionLabel.text = "A Racecar Sponsored by Swift"
+        } else if emoji == "🚔" {
+            emojiDefinitionLabel.text = "Police Car"
+        } else if emoji == "🛩" {
+            emojiDefinitionLabel.text = "Descending Airplane"
+        } else if emoji == "🇦🇷" {
+            emojiDefinitionLabel.text = "The Argentinian Flag"
+        }
 
     }
 
